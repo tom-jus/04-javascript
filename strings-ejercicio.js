4f15b833cccd0513d35c4d86aabf82883b02046d
tom-jus <101270851+tom-jus@users.noreply.github.com>
2022-08-24 14:19:32 -0300

    Add files via upload

---
+++ strings-ejercicio.js
@@ -0,0 +1,24 @@
+
+let first_name = "Tomas";
+let last_name = "Justel";
+let student = "Estudiante";
+student.concat(" ", first_name," ", last_name);
+let studentMayus = "Estudiante en mayusculas";
+let studentMinus = "Estudiante en minusculas";
+studentMayus.toUpperCase();
+studentMinus.toLowerCase();
+console.log(student.length); // Numero 10
+console.log(first_name[0]); // Letra T
+console.log(last_name.charAt(5)); // Letra L 
+console.log(student.trim().length); // Numero 10
+console.log(student.includes(`${first_name}`)); // false
+
+//A algunos puntos del ejercicio les agregue console.log() y a otros no para variar.
+
+
+
+
+
+
+
+
